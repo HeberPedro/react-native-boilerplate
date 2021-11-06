7370bf71cb099ae834d49d5c6b554c7b1f1a69eb
heberpedro <heberv.pedro@gmail.com>
2021-11-06 18:49:09 -0300

    test: home page

--- src/__tests__/pages/Home/index.spec.tsx
+++ src/__tests__/pages/Home/index.spec.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { render } from '@testing-library/react-native'
 
-import Home from '../../pages/Home'
+import Home from '../../../pages/Home'
 
 describe('[PAGE]: Home', () => {
-  it('it should be render Home page', () => {
+  it('should be render Home page', () => {
     render(<Home />)
   })
 })
