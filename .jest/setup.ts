3dfc79bd8b3085944981056916fb25597679930e
heberpedro <heberv.pedro@gmail.com>
2021-11-06 18:52:32 -0300

    fix: animated: 'useNativeDriver' is not supported because the native animated module is missing - test

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,3 +1,5 @@
 import 'jest-styled-components'
 import '@testing-library/react-native'
 import '@testing-library/jest-native/extend-expect'
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
