db39f4375e42a280715e738f86656826dcd711d7
heberpedro <heberv.pedro@gmail.com>
2021-11-06 18:48:48 -0300

    test: app root

---
+++ src/__tests__/App.spec.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { render } from '@testing-library/react-native'
+
+import App from '../App'
+
+describe('APP', () => {
+  test('should render App', () => {
+    render(<App />)
+  })
+})
