0cde1fac606b565bfcd7f2526983ee9064957b1a
heberpedro <heberv.pedro@gmail.com>
2021-11-07 11:48:42 -0300

    fix: react-native link

--- ios/ReactNativeBoilerplateTests/ReactNativeBoilerplateTests.m
+++ ios/ReactNativeBoilerplateTests/ReactNativeBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface test123Tests : XCTestCase
+@interface ReactNativeBoilerplateTests : XCTestCase
 
 @end
 
-@implementation test123Tests
+@implementation ReactNativeBoilerplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
